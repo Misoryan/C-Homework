04b5f0c6516fa155b1d6fb64e61a566c7efe5989
Misoryan <2210615612@qq.com>
2023-03-07 19:49:12 +0800

    Initial commit

---
+++ ACM_2315_2.c
@@ -0,0 +1,17 @@
+//
+// Created by 22106 on 2023/3/7.
+//
+#include <stdio.h>
+
+int main() {
+    int a;
+    char b;
+    scanf("%d",&a);
+    for (int i = 0; i < a; ++i) {
+        for (int j = 0; j < (i+1); ++j) {
+            b = 65 + i;
+            printf("%c",b);
+        }
+        printf("\n");
+    }
+}
